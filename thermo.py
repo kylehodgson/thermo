9892b24f953d037b5898dbf3d1426ef1c04be5d7
Kyle H <kyleh.recovery@gmail.com>
2021-12-22 16:47:23 -0500

    working

--- thermo.py
+++ thermo.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-from time import sleep
+import time
 import os
 import sys
+import asyncio
 
 from bleson import get_provider, Observer, UUID16
 from bleson.logger import log, set_level, ERROR, DEBUG
+from kasa import SmartPlug
 
 # Disable warnings
 set_level(ERROR)
@@ -25,6 +27,42 @@ govee_devices = {}
 # ###########################################################################
 FORMAT_PRECISION = ".2f"
 
+check_interval=int(5)
+last_reading=int(time.time()) - (check_interval * 2)
+async def process_thermo(reading):
+    global last_reading, check_interval
+
+    this_reading=int(time.time())
+    elapsed=this_reading - last_reading 
+
+    if elapsed < check_interval:
+        return
+    
+    if reading['name']=="GVH5075_E666":
+        location="office"
+    else:
+        location="garage"
+    
+    last_reading=this_reading
+    print(f"Location: {location} temp: {reading['temp']} humidity: {reading['humidity']} battery: {reading['battery']}")
+
+    if location=="garage":
+        return
+    
+    desired=float(20)
+    acceptable_drift=float(1)
+    heater_ip="192.168.2.46"
+    
+    p=SmartPlug(heater_ip)
+    await p.update()
+    
+    if reading['name']=='GVH5075_E666' and p.is_off and float(reading['temp']) < desired-acceptable_drift :
+        print(f"temp {reading['temp']} is unacceptably cool, turning on panel")
+        await p.turn_on()
+
+    if reading['name']=='GVH5075_E666' and p.is_on and float(reading['temp']) > desired+acceptable_drift :
+        print(f"temp {reading['temp']} is unacceptably warm, turning off panel")
+        await p.turn_off()
 
 # Decode H5075 Temperature into degrees Celcius
 def decode_temp_in_c(encoded_data):
@@ -54,8 +92,6 @@ Battery:  {govee_device['battery']}%"
 def print_rssi(mac):
     govee_device = govee_devices[mac]
     outstr=""
-    #for key, value in govee_device.items() :
-    #    print(f"key {key}, value {value}")
     if "rssi" in govee_device.keys():
         outstr+=" RSSI: " + str(govee_device['rssi'])
     if "address" in govee_device.keys():
@@ -76,18 +112,19 @@ def on_advertisement(advertisement):
             name = advertisement._name
             encoded_data = int(advertisement.mfg_data.hex()[6:12], 16)
             battery = int(advertisement.mfg_data.hex()[12:14], 16)
-            govee_devices[mac]["address"] = mac
-            govee_devices[mac]["name"] = name
-            govee_devices[mac]["mfg_data"] = advertisement.mfg_data
-            govee_devices[mac]["data"] = encoded_data
+            # govee_devices[mac]["address"] = mac
+            # govee_devices[mac]["name"] = name
+            # govee_devices[mac]["mfg_data"] = advertisement.mfg_data
+            # govee_devices[mac]["data"] = encoded_data
 
-            govee_devices[mac]["tempInC"] = decode_temp_in_c(encoded_data)
-            govee_devices[mac]["tempInF"] = decode_temp_in_f(encoded_data)
-            govee_devices[mac]["humidity"] = decode_humidity(encoded_data)
+            # govee_devices[mac]["tempInC"] = decode_temp_in_c(encoded_data)
+            # govee_devices[mac]["tempInF"] = decode_temp_in_f(encoded_data)
+            # govee_devices[mac]["humidity"] = decode_humidity(encoded_data)
 
-            govee_devices[mac]["battery"] = battery
-            if name=='GVH5075_E666' :
-                print_values(mac)
+            # govee_devices[mac]["battery"] = battery
+            #print_values(mac)
+            reading={'name': str(name), 'temp': float(decode_temp_in_c(encoded_data)), 'battery': float(battery), 'humidity': float(decode_humidity(encoded_data))}
+            asyncio.run(process_thermo(reading))
 
         if advertisement.rssi is not None and advertisement.rssi != 0:
             govee_devices[mac]["rssi"] = advertisement.rssi
@@ -107,7 +144,7 @@ observer.on_advertising_data = on_advertisement
 try:
     while True:
         observer.start()
-        sleep(2)
+        time.sleep(2)
         observer.stop()
 except KeyboardInterrupt:
     try:
