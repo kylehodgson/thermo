76a02ce706be8d94a592201febecd5863d7ebff9
Kyle H <kyleh.recovery@gmail.com>
2022-01-05 23:48:58 -0500

    adding UI capabilities for saving new desired temperature

---
+++ README.md
@@ -0,0 +1,8 @@
+# Install
+
+pip install -r requirements.txt
+
+# Run
+
+python thermo.py
+uvicorn main:app --reload --host 0.0.0.0 --port 8888
--- http_svc/main.py
+++ http_svc/main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 from models import TemperatureSetting
+from jinja2 import Template
 import sensor_service as sensor
 
 app = FastAPI()
 origins=["http://192.168.2.21"]
 app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
-
 
 @app.get("/")
 async def root():
@@ -16,6 +17,12 @@ async def root():
 async def getConfig():
     return sensor.getSensorConfig()
 
+@app.get("/config-hx/", response_class=HTMLResponse)
+async def getConfig():
+    with open('show_configs.jinja') as f:
+        template = Template(f.read())
+    return template.render(config=sensor.getSensorConfig())
+
 @app.get("/config/{code}")
 async def getConfigFor(code: str):
     return sensor.getSensorConfig()[code]
@@ -24,6 +31,21 @@ async def getConfigFor(code: str):
 async def getTemperatureFor(code: str):
     return sensor.getSensorConfig()[code]['temp']
 
+@app.get("/config-hx/{code}/temperature", response_class=HTMLResponse)
+async def getTemperatureFor(code: str):
+    temp = sensor.getSensorConfig()[code]['temp']
+    with open('temperature_input.jinja') as f:
+            template = Template(f.read())
+    return template.render(temperature=temp)
+
 @app.post("/config/")
 async def setConfig(p: TemperatureSetting):
     return sensor.setSensorTemp(p.code, p.temperature)
+
+@app.post("/config-hx/", response_class=HTMLResponse)
+async def setConfig(code: str = Form("code"), temperature: str = Form("temperature")):
+    result = sensor.setSensorTemp(code, temperature)
+    with open('temperature_input.jinja') as f:
+        template = Template(f.read())
+    return template.render(temperature=result['temp'])
+
