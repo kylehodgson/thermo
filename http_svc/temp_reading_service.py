e1cb9d98c3c3ae3c015d8f051bcb16b007d816cd
Kyle Hodgson <kyle.hodgson@gmail.com>
2022-01-06 23:07:51 -0500

    adding temp cache files to gitignore

---
+++ http_svc/temp_reading_service.py
@@ -0,0 +1,13 @@
+import json
+
+READING_DIR='/home/pi/projects/thermo/'
+
+def getTemperatureReading(sensor: str):
+    if not sensor:
+        return False
+    filename=READING_DIR + "thermo-state-" + sensor + ".json"
+    with open(filename) as f:
+        state=json.load(f)
+    return state['temp']
+
+
