280c799e1bcb5b87899101f2e207c6bd5c6964db
Kyle Hodgson <kyle.hodgson@gmail.com>
2022-01-25 11:22:55 -0500

    adding an http service for config

---
+++ config_http_service.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+SERVER="http://linuxdev.local:8888/"
+
+def load_config():
+    try:
+        response = requests.get(SERVER + "/config")
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print("Got unexpected http status for " + SERVER + " in config_http_service.load_config() error: " + e)
+    except e:
+        print("Could not read from server " + SERVER + " in config_http_service.load_config() error: " + e)
+        return
+    return response.json()
