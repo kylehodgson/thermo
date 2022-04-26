bcb1dde4fb20169e276e742fc469327c4f6cce2e
Kyle Hodgson <kyle.hodgson@gmail.com>
2022-04-26 08:34:11 -0400

    fixing vagrant provision script

--- scripts/vagrant-provision.sh
+++ scripts/vagrant-provision.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sudo apt -y update
-sudo apt -y install postgresql postgresql-contrib libpq-dev
-sudo apt -y install python3
+sudo apt-get -y update
+sudo apt-get -y install postgresql postgresql-contrib libpq-dev
+sudo apt-get -y install python3
 sudo setcap cap_net_raw,cap_net_admin+eip $(eval readlink -f `which python3`)
 sudo systemctl start postgresql
 sudo -u postgres createdb thermo
 sudo -u postgres createuser zonemgr
 sudo -u postgres psql thermo -f /vagrant/db/migration-0001.sql
-. /vagrant/example.env
 pip3 install -r /vagrant/requirements.txt
 cd /vagrant
-/home/vagrant/.local/bin/uvicorn zonemgr.api:app --host 0.0.0.0 --port 8888 &
+. example.env
+uvicorn zonemgr.api:app --host 0.0.0.0 --port 8888 &
