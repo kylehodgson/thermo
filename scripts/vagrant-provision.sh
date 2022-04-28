ac5ecb5a2e74514f76734e331b570cec44bfdb4b
Kyle Hodgson <kyle.hodgson@gmail.com>
2022-04-28 09:32:34 -0400

    working on a more secure vagrant provision

--- scripts/vagrant-provision.sh
+++ scripts/vagrant-provision.sh
@@ -9,5 +9,8 @@ sudo -u postgres createuser zonemgr
 sudo -u postgres psql thermo -f /vagrant/db/migration-0001.sql
 pip3 install -r /vagrant/requirements.txt
 cd /vagrant
-. example.env
-uvicorn zonemgr.api:app --host 0.0.0.0 --port 8888 &
+somechars=$(LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c 24 ; echo)
+sed -e s/\|CHANGEME\|/${somechars}/g < example.env > .env
+sudo -u postgres psql thermo -c "ALTER USER zonemgr WITH PASSWORD '${somechars}'"
+. .env
+uvicorn zonemgr.api:app --host 0.0.0.0 --port 8888 --reload &
