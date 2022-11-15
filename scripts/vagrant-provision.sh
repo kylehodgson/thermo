661c611f73c818ca3e89bfd78851c57102439103
Kyle Hodgson <kyle.hodgson@gmail.com>
2022-11-14 21:01:24 -0500

    tweaking apt-get

--- scripts/vagrant-provision.sh
+++ scripts/vagrant-provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get -y update
-sudo apt-get -y install postgresql postgresql-contrib libpq libpq-dev
+sudo apt-get -y install postgresql postgresql-contrib libpq-dev
 sudo apt-get -y install python3 python3-venv python3-pip
 sudo setcap cap_net_raw,cap_net_admin+eip $(eval readlink -f `which python3`)
 sudo systemctl start postgresql
@@ -42,9 +42,11 @@ function installApp() {
     python3 -m venv venv
     . venv/bin/activate
     pip3 install -r requirements.txt
+    
     somechars=$(LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c 24 ; echo)
     sed -e s/\|CHANGEME\|/${somechars}/g < example.env > .env
     sudo -u postgres psql thermo -c "ALTER USER zonemgr WITH PASSWORD '${somechars}'"
+    
     . ./.env
     for sql in $(ls db/*.sql)
     do
